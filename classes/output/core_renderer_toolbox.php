f44fe0a42aac887bd2d91aaf370f8ab20336ee70
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2019-09-24 18:14:15 +0100

    Tidy.

--- classes/output/core_renderer_toolbox.php
+++ classes/output/core_renderer_toolbox.php
@@ -126,6 +126,21 @@ trait core_renderer_toolbox {
     }
 
     /**
+     * Outputs the opening section of a box.
+     *
+     * @param string $classes A space-separated list of CSS classes
+     * @param string $id An optional ID
+     * @param array $attributes An array of other attributes to give the box.
+     * @return string the HTML to output.
+     */
+    public function box_start($classes = 'generalbox', $id = null, $attributes = array()) {
+        $this->opencontainers->push('box', html_writer::end_tag('div'));
+        $attributes['id'] = $id;
+        $attributes['class'] = 'box ' . \renderer_base::prepare_classes($classes);
+        return html_writer::start_tag('div', $attributes);
+    }
+
+    /**
      * Prints a nice side block with an optional header.
      *
      * @param block_contents $bc HTML for the content
