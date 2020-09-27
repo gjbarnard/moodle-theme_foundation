5b7f4c1ae81f0f14678c4d59cf21777184b44471
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2020-09-27 23:22:12 +0100

    Move the context cog to the navbar.

--- classes/output/core_renderer_toolbox.php
+++ classes/output/core_renderer_toolbox.php
@@ -48,6 +48,19 @@ trait core_renderer_toolbox {
         $bodyclasses = array();
         $regionmainsettingsmenu = $this->region_main_settings_menu();
 
+        if ($this->page->include_region_main_settings_in_header_actions() &&
+                !$this->page->blocks->is_block_present('settings')) {
+            // Only include the region main settings if the page has requested it and it doesn't already have
+            // the settings block on it. The region main settings are included in the settings block and
+            // duplicating the content causes behat failures.
+            $this->page->add_header_action(html_writer::div(
+                $this->region_main_settings_menu(),
+                'd-print-none',
+                ['id' => 'region-main-settings-menu']
+            ));
+        }
+        $contextheadersettingsmenu = $this->context_header_settings_menu();
+
         if (!empty($this->page->theme->layouts[$this->page->pagelayout]['regions'])) {
             if (in_array('drawer', $this->page->theme->layouts[$this->page->pagelayout]['regions'])) {
                 $drawerblockshtml = $this->blocks('drawer');
@@ -123,9 +136,27 @@ trait core_renderer_toolbox {
         $data->bodyattributes = $this->body_attributes($bodyclasses);
         $data->regionmainsettingsmenu = $regionmainsettingsmenu;
         $data->hasregionmainsettingsmenu = !empty($regionmainsettingsmenu);
+        $data->contextheadersettingsmenu = $contextheadersettingsmenu;
+        $data->hascontextheadersettingsmenu = !empty($contextheadersettingsmenu);
         $data->fav = !empty($toolbox->get_setting('fav'));
 
         echo $this->render_from_template('theme_foundation/'.$mustache, $data);
+    }
+
+    /**
+     * Wrapper for header elements.
+     *
+     * @return string HTML to display the main header.
+     */
+    public function full_header() {
+        $header = new \stdClass();
+        $header->contextheader = $this->context_header();
+        $header->hasnavbar = empty($this->page->layout_options['nonavbar']);
+        $header->navbar = $this->navbar();
+        $header->pageheadingbutton = $this->page_heading_button();
+        $header->courseheader = $this->course_header();
+        $header->headeractions = $this->page->get_header_actions();
+        return $this->render_from_template('core/full_header', $header);
     }
 
     /**
