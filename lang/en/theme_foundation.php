c23daaf81fb6f73ace62dc8d2d5fdb027e4a66ed
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-02-05 19:55:15 +0000

    Tabs in tabs don't save the settings on them.

--- lang/en/theme_foundation.php
+++ lang/en/theme_foundation.php
@@ -233,11 +233,15 @@ $string['moduleheadingdesc'] = 'Configure the module settings for Foundation her
 /* Future code? -> $toolbox = \theme_foundation\toolbox::get_instance();
    $string = array_merge($string, $toolbox->get_lang_strings('en')); */
 // Module strings here for now!
-$string['swatch'] = 'Swatch';
-$string['swatchdesc'] = 'Choose the swatch for the theme.  A \'Swatch\' is a way of changing the look of the theme using a preset list of definitions that you attach a name to.  All swatches (bar \'Seventies\' which I created) are from \'Bootswatch.com\' and licensed under the \'MIT License\'.  Note:  The Google font CDN\'s have been removed due to limitations with the PHP SCSS compiler and I don\'t want to have the complications of updating the privacy too.';
 $string['swatchheading'] = 'Swatch';
 $string['swatchheadingsub'] = 'Swatch settings';
 $string['swatchheadingdesc'] = 'Configure the swatch settings for Foundation here.';
+$string['swatch'] = 'Swatch';
+$string['swatchdesc'] = 'Choose the swatch for the theme.  A \'Swatch\' is a way of changing the look of the theme using a preset list of definitions that you attach a name to.  All swatches (bar \'Seventies\' which I created) are from \'Bootswatch.com\' and licensed under the \'MIT License\'.  Note:  The Google font CDN\'s have been removed due to limitations with the PHP SCSS compiler and I don\'t want to have the complications of updating the privacy too.';
+$string['custom'] = 'Custom';
+$string['swatchcustomheading'] = 'Custom';
+$string['swatchcustomheadingsub'] = 'Custom swatch settings';
+$string['swatchcustomheadingdesc'] = 'Configure the custom swatch settings for Foundation here.';
 
 // Accessibility.
 $string['navbarmenus'] = 'Navbar menus';
