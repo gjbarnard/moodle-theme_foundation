5f1679f7f9dd312c4151fd438928f5738a2c6c98
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-02-08 23:56:55 +0000

    Anti gravity work in progress.

--- amd/src/anti_gravity.js
+++ amd/src/anti_gravity.js
@@ -14,23 +14,56 @@ define(['jquery', 'core/log'], function($, log) {
     log.debug('Foundation anti gravity AMD initialised');
 
     $(document).ready(function() {
-        var offset = 220;
+        var topoffset = 220;
         var duration = 1000;
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > offset) {
-                $('#backtotop').fadeIn(duration);
-            } else {
-                $('#backtotop').fadeOut(duration);
-            }
-        });
+        var botoffset = $(document).height() - $(window).height();
+        var btt = false;
+        var gtb = false;
 
-        $('#backtotop').click(function(event) {
+        var bscheck = function () {
+            if (btt && gtb) {
+                $('.footerantigravity').addClass('bothshown');
+            } else {
+                $('.footerantigravity').removeClass('bothshown');
+            }
+        };
+
+        var gravitycheck = function () {
+            var sc = $(window).scrollTop();
+            if (sc > topoffset) {
+                btt = true;
+                bscheck();
+                $('.backtotop').fadeIn(duration);
+            } else {
+                btt = false;
+                $('.footerantigravity .backtotop').fadeOut(duration, bscheck);
+                $('.navbarantigravity.backtotop').fadeOut(duration);
+            }
+            if (sc < botoffset) {
+                gtb = true;
+                if (btt) {
+                    bscheck();
+                }
+                $('.gotobottom').fadeIn(duration);
+            } else {
+                gtb = false;
+                $('.footerantigravity .gotobottom').fadeOut(duration, bscheck);
+                $('.navbarantigravity.gotobottom').fadeOut(duration);
+            }
+        };
+
+        $(window).scroll(function () {
+            gravitycheck();
+        });
+        gravitycheck();
+
+        $('.backtotop').click(function(event) {
             event.preventDefault();
             $('html, body').animate({scrollTop: 0}, duration);
             return false;
         });
 
-        $('#gotobottom').click(function(e) {
+        $('.gotobottom').click(function(e) {
             e.preventDefault();
             var target = $('#page-footer');
             $('html, body').animate({scrollTop: target.position().top}, duration);
