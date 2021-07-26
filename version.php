4c6adc9f86a375da5a6d408c7c077c8c11296a50
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-07-26 17:27:37 +0100

    Version supported.

--- version.php
+++ version.php
@@ -27,6 +27,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2021051801;
 $plugin->requires  = 2021051700.00; // 3.11 (Build: 20210517).
+$plugin->supported = array(311, 311);
 $plugin->component = 'theme_foundation';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.11.1.1';
