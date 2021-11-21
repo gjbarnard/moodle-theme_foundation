ab69259d90eaf5106eca2790be102534b8091eac
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-11-21 17:12:09 +0000

    V3.10.1.7

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2020110807;
+$plugin->version   = 2020110808;
 $plugin->requires  = 2020110900.00; // 3.10 (Build: 20201109).
 $plugin->supported = array(310, 310);
 $plugin->component = 'theme_foundation';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.10.1.6';
+$plugin->release = '3.10.1.7';
 $plugin->dependencies = array(
     'theme_boost'  => 2020110900
 );
