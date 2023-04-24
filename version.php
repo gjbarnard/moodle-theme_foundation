1249285888a7e6a78432a512ac61b1b57875b39d
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-04-24 18:34:51 +0100

    402.0.1 work in progress.

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2023042200;
-$plugin->requires = 2023041800.00; // 4.2rc1 (Build: 20230418).
+$plugin->version = 2023042400;
+$plugin->requires = 2023042400.00; // 4.2 (Build: 20230424).
 $plugin->supported = array(402, 402);
 $plugin->component = 'theme_foundation';
 $plugin->maturity = MATURITY_RC;
 $plugin->release = '402.0.1';
 $plugin->dependencies = array(
-    'theme_boost' => 2022112800
+    'theme_boost' => 2023042400
 );
