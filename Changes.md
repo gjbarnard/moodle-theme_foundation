a5671f3d2db367d5f5589136d708056f6aa21a03
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2023-09-25 13:31:50 +0100

    V401.1.3

--- Changes.md
+++ Changes.md
@@ -1,6 +1,9 @@
 Version Information
 ===================
 
+Version 401.1.3 - TBR
+--------------------------
+
 Version 401.1.2 - 08/09/23
 --------------------------
 1. Fix 'Logout Url not working' - https://moodle.org/mod/forum/discuss.php?d=448979.
--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2022112603;
+$plugin->version = 2022112604;
 $plugin->requires = 2022112800.00; // 4.1 (Build: 20221128).
 $plugin->supported = array(401, 401);
 $plugin->component = 'theme_foundation';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '401.1.2';
+$plugin->release = '401.1.3';
 $plugin->dependencies = array(
     'theme_boost' => 2022112800
 );
