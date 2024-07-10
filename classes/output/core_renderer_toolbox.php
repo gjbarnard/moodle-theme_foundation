db93051b4e89ff0c00698e14695316a8e291bd1d
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2024-07-10 11:17:41 +0100

    Accessible block title.

--- classes/output/core_renderer_toolbox.php
+++ classes/output/core_renderer_toolbox.php
@@ -312,9 +312,10 @@ trait core_renderer_toolbox {
         ];
 
         if ($this->page->blocks->region_has_content($displayregion, $this)) {
-            $content .= $this->blocks_for_region($displayregion, $fakeblocksonly);
+            $content .= html_writer::tag('h2', get_string('blocks'), ['class' => 'sr-only']) .
+                $this->blocks_for_region($displayregion, $fakeblocksonly);
         } else {
-            $content .= '';
+            $content .= html_writer::tag('h2', get_string('blocks'), ['class' => 'sr-only']);
         }
 
         if ($region != 'content') {
