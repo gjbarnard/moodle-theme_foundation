35e0280d7b7ccc9067043a26471bcf401af7f8ea
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2024-07-19 19:01:20 +0100

    Collapse Block -> ES6.

--- amd/src/collapseblock.js
+++ amd/src/collapseblock.js
@@ -13,40 +13,58 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+//
+// Collapse block.
+//
+// @module     theme_foundation/collapseblock
+// @copyright  2019 G J Barnard.
+// @author     G J Barnard -
+//               {@link https://moodle.org/user/profile.php?id=442195}
+//               {@link https://gjbarnard.co.uk}
+// @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+//
+
+import $ from 'jquery';
+import * as FoundationUtil from 'theme_foundation/util';
+import log from 'core/log';
+
 /**
- * Collapse block.
- *
- * @module     theme_foundation/collapseblock
- * @copyright  2019 G J Barnard.
- * @author     G J Barnard -
- *               {@link https://moodle.org/user/profile.php?id=442195}
- *               {@link https://gjbarnard.co.uk}
- * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
+ * Collapse Block class.
  */
+class CollapseBlock {
+    static noInit = true;
 
-/* jshint ignore:start */
-define(['jquery', 'core/log', 'theme_foundation/util'], function($, log, FoundationUtil) {
+    static collapseBlock() {
+        if (CollapseBlock.noInit) {
+            CollapseBlock.noInit = false;
+            $('.block-collapsible').click(function() {
+                var instanceId = $(this).data('instanceid');
+                var blockInstance = $('#inst' + instanceId);
 
-    "use strict"; // jshint ;_;
-
-    log.debug('Foundation Collapse Block AMD');
-
-    $(document).ready(function($) {
-        $('.block-collapsible').click(function() {
-            var instanceId = $(this).data('instanceid');
-            var blockInstance = $('#inst' + instanceId);
-
-            $('#inst' + instanceId + ' .card-text').slideToggle('slow', function() {
-                if (blockInstance.hasClass('hidden')) {
-                    blockInstance.removeClass('hidden');
-                    FoundationUtil.setUserPreference('block' + instanceId + 'hidden', 0);
-                } else {
-                    blockInstance.addClass('hidden');
-                    FoundationUtil.setUserPreference('block' + instanceId + 'hidden', 1);
-                }
+                $('#inst' + instanceId + ' .content').slideToggle('slow', function() {
+                    if (blockInstance.hasClass('hidden')) {
+                        blockInstance.removeClass('hidden');
+                        FoundationUtil.setUserPreference('block' + instanceId + 'hidden', 0);
+                    } else {
+                        blockInstance.addClass('hidden');
+                        FoundationUtil.setUserPreference('block' + instanceId + 'hidden', 1);
+                    }
+                });
             });
+            log.debug('Foundation Collapse Block ES6 collapseBlock');
+        }
+    }
+}
+
+/**
+ * Init.
+ */
+export const collapseBlockInit = () => {
+    if (document.readyState !== 'loading') {
+        CollapseBlock.collapseBlock();
+    } else {
+        document.addEventListener('DOMContentLoaded', function () {
+            CollapseBlock.collapseBlock();
         });
-        log.debug('Foundation Collapse Block AMD init');
-    });
-});
-/* jshint ignore:end */
+    }
+};
